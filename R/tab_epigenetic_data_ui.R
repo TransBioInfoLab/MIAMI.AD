6abbfe004ea27419a6fc2bb052417f528fd2b090
dlukacso <dlukacsovich@gmail.com>
2024-05-02 15:05:39 -0400

    eforge removed
    Removed eforge link button and text

--- R/tab_epigenetic_data_ui.R
+++ R/tab_epigenetic_data_ui.R
@@ -9,22 +9,8 @@ tab_epigenetic_data_UI <- function(id) {
         ns("download_data"),
         "Download Tables",
         width = "150px"
-      ),
-      shiny::actionButton(
-        inputId = 'link_eforge',
-        label = "eFORGE",
-        icon = icon("th"),
-        width = "150px",
-        onclick = "window.open('https://eforge.altiusinstitute.org/', '_blank')"
       )
     ),
-    shiny::tags$p(
-      "To perform gene ontology analysis, click on ",
-      shiny::tags$em("Download Tables"),
-      " to get the list of CpGs, and use those on ",
-      shiny::tags$em("eFORGE")
-    ),
-    
 
     shiny::h4("Selected Epigenetic Clocks", class = "top-margin-m"),
     DT::DTOutput(ns("data_selection_data")) %>%
